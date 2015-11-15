638cc4524345cb1bcd61181aaaa57ccd093d81d7
venkatkollikonda <venkat.kollikonda@fissionlabs.in>
2015-11-15 10:35:25 +0800

    key update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,7 +12,7 @@
 
 angular.module('HotelReview', ['ionic', 'ngCordova', 'ngResource', 'ngSanitize'])
     .constant("accessKey", {
-        "key": "AIzaSyC12UlFrlR4o57BJVsbKhcf43ECn7UuUAM",
+        "key": "AIzaSyABcy-mbYnR10GMp_BMrzaTq2lnh-BKtt4",
         "servicesUrl":"https://maps.googleapis.com/maps/api/place"
     })
 
